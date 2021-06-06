d3deba85677b6bd45805fff40ccfaecdbd3b4254
floaterD <florian22jungwirth@gmail.com>
2021-06-06 23:57:52 +0200

    Test

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 window.addEventListener("deviceorientation", handleOrientation, true);
+let box = document.querySelector('#box')
 
 function handleOrientation(event) {
-    var absolute = event.absolute;
-    var alpha    = event.alpha;
-    var beta     = event.beta;
-    var gamma    = event.gamma;
-  
-    document.body.innerHTML = `alpha: ${Math.round(alpha)}, beta: ${Math.round(beta)}, gamma: ${Math.round(gamma)}`;
-  }
+  var absolute = event.absolute;
+  var alpha = event.alpha;
+  var beta = event.beta;
+  var gamma = event.gamma;
+
+  box.innerHTML = `alpha: ${Math.round(alpha)}, beta: ${Math.round(beta)}, gamma: ${Math.round(gamma)}`;
+
+  box.style.transform = `rotateZ(${beta}deg)`;
+}
