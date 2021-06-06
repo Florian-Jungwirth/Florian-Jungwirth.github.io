67531b86bc68b727ba98254f43050dfa94702ab2
floaterD <florian22jungwirth@gmail.com>
2021-06-06 21:40:39 +0200

    Test

--- script.js
+++ script.js
@@ -1,34 +1,10 @@
-let navContent = document.getElementById('navContent');
-let scrollDown = document.getElementById('scrollDown');
+window.addEventListener("deviceorientation", handleOrientation, true);
 
-function changeMenu(x) {
-    x.classList.toggle('change');
-    if(x.classList.value === 'change') {
-        navContent.style.opacity = 1;
-        navContent.style.zIndex = 9;
-        navContent.style.position = 'sticky';
-        document.body.style.overflow = 'hidden';
-        if(scrollDown !== null) {scrollDown.style.zIndex = -10;}
-    } else {
-        navContent.style.opacity = 0;
-        navContent.style.zIndex = -9;
-        navContent.style.position = 'absolute';
-        document.body.style.overflow = 'visible';
-        scrollDown.style.zIndex = 10;
-    }
-}
- 
-window.addEventListener('load', function() {
-    window.onscroll = function () {
-
-        if(scrollDown !== null) {
-            if(window.pageYOffset < 100) {
-                scrollDown.style.opacity = 1;
-                scrollDown.style.position = 'fixed';
-            } else {
-                scrollDown.style.opacity = 0;
-                scrollDown.style.position = 'absolute';
-            }
-        }
-    };
-});
+function handleOrientation(event) {
+    var absolute = event.absolute;
+    var alpha    = event.alpha;
+    var beta     = event.beta;
+    var gamma    = event.gamma;
+  
+    document.body.innerHTML = absolute;
+  }
