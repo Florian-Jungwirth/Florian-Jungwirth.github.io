47ca5a6aecabd33264bbf6d3944fff26a6953f53
floaterD <florian22jungwirth@gmail.com>
2021-06-07 08:16:42 +0200

    Test

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ let box = document.querySelector('#box')
 
 function handleOrientation(event) {
   var absolute = event.absolute;
-  var alpha = event.alpha;
+  var alpha = event.alpha; 
   var beta = event.beta;
   var gamma = event.gamma;
 
   box.innerHTML = `alpha: ${Math.round(alpha)}, beta: ${Math.round(beta)}, gamma: ${Math.round(gamma)}`;
 
-  box.style.transform = `rotateZ(${beta}deg)`;
+  box.style.transform = `rotateZ(${-beta}deg)`;
 }
